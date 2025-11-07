28a5f184176b1951b40c0cec28a6cb4a85186688
Angel Gonzalez M. <119265574+xNeuNoRo@users.noreply.github.com>
2025-11-06 21:27:27 -0400

    feat: add dynamic route configuration for orders API to ensure fresh data retrieval

--- app/admin/orders/api/route.ts
+++ app/admin/orders/api/route.ts
@@ -1,5 +1,9 @@
 import { prisma } from "@/src/lib/db";
 
+// Forzar que esta ruta sea dinamica
+// asi siempre se obtienen datos actualizados sin el cacheo intenso de nextjs
+export const dynamic = "force-dynamic";
+
 // Hay que exportar la funcion como un metodo HTTP para que Next la reconozca como endpoint
 // Todo esto es del servidor, no del cliente, por ende es seguro hacer consultas a la base de datos aqui
 export async function GET() {
--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -1,5 +1,9 @@
 import { prisma } from "@/src/lib/db";
 
+// Forzar que esta ruta sea dinamica
+// asi siempre se obtienen datos actualizados sin el cacheo intenso de nextjs
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const orders = await prisma.order.findMany({
     take: 5,
