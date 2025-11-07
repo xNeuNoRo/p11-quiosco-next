67404f6569e782e282e7006b223f38c57dbf4744
Angel Gonzalez M. <119265574+xNeuNoRo@users.noreply.github.com>
2025-11-06 21:24:03 -0400

    feat: integrate Prisma adapter for PostgreSQL in db.ts

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: { optimizePackageImports: [] },
 };
 
 export default nextConfig;
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@/src/lib/prisma/generated/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+// crear el pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// crear el adapter
+const adapter = new PrismaPg(pool);
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalThis.prisma || new PrismaClient();
+export const prisma = globalThis.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
